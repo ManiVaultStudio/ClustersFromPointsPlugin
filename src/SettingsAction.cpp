1e8e90024a87adccd5050a2569cbdb8128d22c20
Thomas Kroes <t.kroes@lumc.nl>
2023-03-04 15:04:59 +0100

    Adhere to new group action API

--- src/SettingsAction.cpp
+++ src/SettingsAction.cpp
@@ -6,7 +6,7 @@
 using namespace hdps;
 
 SettingsAction::SettingsAction(ClustersFromPointsPlugin* clustersFromPointsPlugin) :
-    GroupAction(nullptr, true),
+    GroupAction(nullptr, "Settings", true),
     _clustersFromPointsPlugin(clustersFromPointsPlugin),
     _clustersAction(this),
     _algorithmAction(*this)
@@ -15,11 +15,9 @@ SettingsAction::SettingsAction(ClustersFromPointsPlugin* clustersFromPointsPlugi
 
     _clustersAction.setVisible(false);
 
-    // Sort actions
-    _algorithmAction.setSortIndex(1);
-    _clustersAction.setSortIndex(2);
+    addAction(&_algorithmAction);
+    addAction(&_clustersAction);
 
-    // Set widget layout for clusters action
     _clustersAction.setDefaultWidgetFlags(ClustersAction::Filter | ClustersAction::Select | ClustersAction::Remove | ClustersAction::Merge | ClustersAction::Subset | ClustersAction::Colorize | ClustersAction::Prefix);
 }
 
